7d2ad523540d16049d9d6a176ae1914c7b90bbf7
Davide <kalelc351k@gmail.com>
2021-02-03 18:05:30 +0100

    qualche commento

--- project-laravel-drink-crud/routes/web.php
+++ project-laravel-drink-crud/routes/web.php
@@ -10,7 +10,9 @@ Route::get('/show/{id}', 'MainController@show') -> name('show-route');
 Route::get('/create', 'MainController@create') -> name('create-route');
 Route::post('/store', 'MainController@store') -> name('store-route');
 
+// rotte di edit e update di ció che si sta aggiornando
 Route::get('/edit/{id}', 'MainController@edit') -> name('edit-route');
 Route::post('/update/{id}', 'MainController@update') -> name('update-route');
 
+// rotta di eliminazione di un elemento
 Route::get('/delete/{id}', 'MainController@delete') -> name('delete-route');
